ceb1169905422e3dbe9b0b7f548be55d5184e8f6
gaia-papaya <car9715@nyu.edu>
2024-01-18 13:41:47 -0500

    ok this is the final analysis of redlining for realsies

--- Rscripts/RedLiningGIS.R
+++ Rscripts/RedLiningGIS.R
@@ -3,6 +3,25 @@ require(tidyverse)
 require(tidycensus)
 require(sf)
 
+#load redlining csv
+RedLine <- read_csv("Rdata/GIS/500mIntersectRedline.csv") %>% 
+  mutate(gradeScore = case_when(grade == "A" ~ 1, #assign grade scores based on grade, with poorer grades = higher score
+                                grade == "B" ~ 2,
+                                grade == "C" ~ 3,
+                                grade == "D" ~ 4,
+                                grade == "E" ~ 5,
+                                grade == NA ~ NA))
+
+#Plot EDA----
+
+ggplot(data = RedLine, aes(x = name311, y = gradeScore)) +
+  geom_violin() +
+  stat_summary(geom = "point", fun.y = mean)
+
+
+
+
+#tidycensus bullshit----
 #load variables bc US census uses the worst system know to humanity----
 vars_avail<- load_variables(year = 2021, dataset = "acs1")
 
