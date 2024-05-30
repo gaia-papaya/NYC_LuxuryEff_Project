d50280f86f4c83bf26067b158c42dddfdf485174
gaia-papaya <car9715@nyu.edu>
2024-05-29 21:27:26 -0400

    finalized taxa scrubber :) it works sexily

--- Rscripts/BirdProcessing.R
+++ Rscripts/BirdProcessing.R
@@ -2,11 +2,11 @@
 require(tidyverse)
 require(taxize)
 
-#set up API key for NCBI
-ENTREZ_KEY='a7c29a42a162d4ee6e888d77a73897e12f08'
+#set up API key for NCBI using usethis::edit_r_environ()
+#ENTREZ_KEY='a7c29a42a162d4ee6e888d77a73897e12f08'
 
 #read in bird survey datasheet
-DirtyBirdData <- read.csv("Rdata/Biodiversity/BirdSurveys_2024-05-27.csv") %>% 
+DirtyBirdData <- read.csv("Rdata/Biodiversity/BirdSurveys_2024-05-29.csv") %>% 
 mutate(UID = row_number(),#add row number as uid
       Species = str_replace_all(Species, "([:lower:])([:upper:])", "\\1 \\2"), #add space between capital letters
       #standardize dog on and off leash name
@@ -35,7 +35,8 @@ filter(Species == "sparrow"| Species =="*sparrow*"| Species == "unknown"| Specie
 
 #filter out unclear values (those found in DirtyBirds
 CleanBirdData <- DirtyBirdData %>% 
-  filter(!(UID %in% DirtyBirds$UID)) 
+  filter(!(UID %in% DirtyBirds$UID))  %>% 
+  rename(commonNames = Species)
   
 
 #how many unique vals?
@@ -43,13 +44,64 @@ sort(unique(DirtyBirdData$Species))
 sort(unique(CleanBirdData$Species))
 
 #find scientific names for given birds
-BirdSciNames <- taxize::comm2sci(unique(CleanBirdData$Species))
-#for those not found on ncbi, check itis
+BirdSciNames <- taxize::comm2sci(unique(CleanBirdData$commonNames[CleanBirdData$commonNames != "dog on" & CleanBirdData$commonNames != "dog off"]))
 BirdSciNamesFail <- names(Filter(is_empty,BirdSciNames))
-BirdSciNamesTry2 <- taxize::comm2sci(BirdSciNamesFail, db = "itis")
+BirdSciNames <- compact(BirdSciNames)
 
+#for those not found on ncbi, check eol
+BirdSciNamesTry2 <- taxize::comm2sci(BirdSciNamesFail, db = "eol")
+BirdSciNamesFail2 <- names(Filter(is_empty,BirdSciNamesTry2))
+BirdSciNamesTry2 <- compact(BirdSciNamesTry2)
+#append onto one list
+BirdSciNames <- c(BirdSciNames, BirdSciNamesTry2)
 
+#assign correct names into data frame
+inconclusiveNames <- list()
+for(n in 1:length(BirdSciNames)){
+  if(length(BirdSciNames[[n]]) > 1){
+  inconclusiveNames[[n]] <- BirdSciNames[[n]]
+  names(inconclusiveNames)[[n]] <- names(BirdSciNames)[[n]]
+ 
+}
+}
+#names with multiple results, manual fix
+inconclusiveNames<- compact(inconclusiveNames)
+RepairedNames <- list("brown creeper" = inconclusiveNames$`brown creeper`[2],
+                      "ruby crowned kinglet" = inconclusiveNames$`ruby crowned kinglet`[1],
+                      "winter wren" = inconclusiveNames$`winter wren`[1],
+                      "pied billed grebe" = inconclusiveNames$`pied billed grebe`[1],
+                      "brant" = inconclusiveNames$brant[5],
+                      "eastern towhee" = inconclusiveNames$`eastern towhee`[1],
+                      "blue grosbeak" = inconclusiveNames$`blue grosbeak`[1],
+                      "brownheaded cowbird" = 'Molothrus ater') 
+#remove inconclusive names from list
+BirdSciNames <- within(BirdSciNames, rm(list =names(inconclusiveNames)))
+#create tibble with both common names and scientific names
+BirdSciNamesTbl <- tibble(commonNames = names(BirdSciNames), species = unlist(BirdSciNames))
 
-comm2sci("pied billed grebe", db = "itis")
-#taxize fail list:
-# Pied-billed Grebe -- Podilymbus podiceps
+#create tibble with both common names and scientific names
+RepairedNamesTbl <- tibble(commonNames = names(RepairedNames), species = unlist(RepairedNames))
+
+#merge two tibbles, remove intermediaries
+CommSciBirdTbl <- bind_rows(BirdSciNamesTbl, RepairedNamesTbl)
+rm(BirdSciNames, BirdSciNamesTbl, BirdSciNamesFail, BirdSciNamesFail2, BirdSciNamesFail3,
+   BirdSciNamesTry2, BirdSciNamesTry3, RepairedNames, BirdSciNamesTbl)
+#query full taxonomic classif
+TaxonClass <- taxize::classification(CommSciBirdTbl$species, db = "ncbi")
+
+#select only the ranks of order, family, and genus
+for(t in 1:length(TaxonClass)){
+  TaxonClass[[t]] <- TaxonClass[[t]] %>% 
+    select(!id) %>% 
+    filter(rank =="order" | rank == "family" | rank == "genus" | rank == "species") %>% 
+    pivot_wider(names_from = "rank", values_from = "name")
+  
+}
+#bind into a single data frame
+TaxonClassTbl <- do.call(rbind, TaxonClass)
+
+#join table with common names to full classif table
+TaxonClassTbl <- full_join(TaxonClassTbl, CommSciBirdTbl, by = "species")
+
+#join classif table to full data sheet
+CleanBirdData <- full_join(CleanBirdData, TaxonClassTbl, by = "commonNames", relationship = "many-to-one")
